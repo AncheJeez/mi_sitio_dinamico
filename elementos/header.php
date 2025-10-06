3886dacc298577b18df35606228f5ae377fa48f4
AncheJeez <andresjsangon@gmail.com>
2025-10-06 10:36:06 +0200

    Added barra de navegacion Bootstrap

--- elementos/header.php
+++ elementos/header.php
@@ -1,4 +1,23 @@
 <header class="text-center bg-white p-3 rounded shadow-sm">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous" />
   <h1 class="text-primary">🌟 Bienvenido a mi primer sitio PHP 🌟</h1>
   <p class="text-muted">Usando include() por primera vez</p>
 </header>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-info">
+        <div class="container">
+            <a class="navbar-brand cls-con" href="#"> Test btn</a>
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav m-auto">
+                <a class="nav-link active" href="#"> Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="#"> About Us </a>
+                <a class="nav-link" href="#"> Services </a>
+                <a class="nav-link" href="#"> Contact </a>
+                <a class="nav-link" href="#"> Follow Us </a>
+            </div>
+        </div>
+    </nav>
